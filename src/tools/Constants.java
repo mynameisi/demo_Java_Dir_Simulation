961d1edb945106e4b18a9a9465dd21941b71e479
mynameisi <test@nowhere.com>
2013-08-23 23:16:51 +0800

    删除了不必要的文件

--- src/tools/Constants.java
+++
@@ -1,9 +0,0 @@
-package tools;
-public final class Constants {
-	private Constants() {
-	};
-	public final static String inputFile = "./input.txt";
-	public static char ud=0;
-	public static char oe=0;
-	public static int level=0;
-}
